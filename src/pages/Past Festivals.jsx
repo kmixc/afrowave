572b3c31585fbdef1eef99ab16ab7d0f31a716a2
kmixc <oliverkmiec24@gmail.com>
2023-06-12 17:14:38 -0400

    Past Festivals content added

--- src/pages/Past Festivals.jsx
+++ src/pages/Past Festivals.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
-import Video from '../img/AFROWAVE_PROMO_VIDEO.mp4'
+
+//2022
+import Video2022 from '../img/AFROWAVE_PROMO_VIDEO.mp4';
+import lineup_2022 from '../img/past_festivals/AfrowaveTO_Fest2022.jpg'
+
+//2021
+import Video2021 from '../img/past_festivals/PROMO_AFROWAVE_FEST_2022_1.mp4'
+import lineup_2021_1 from '../img/past_festivals/AfrowaveTO_Fest_2021.jpg'
+import lineup_2021_2 from '../img/past_festivals/AfrowaveTO_Fest_2021_2.jpg'
 
 export default function Past_Festivals() {
     return (
         <div>
             <div className='spacer'></div>
             <div class="mx-auto video-section">
-                <p>RELIVE <span className='afrowave-color'>2022</span></p>
+                <p>RELIVE <span className='green-color'>2022</span></p>
                 <div className='video'>
                     <video loop="loop" controls id='Video2022'>
-                        <source src={Video}></source>
+                        <source src={Video2022}></source>
                     </video>
+                </div>
+                <div className='lineup2022'>
+                    <img src={lineup_2022} alt="" />
                 </div>
             </div>
 
-            <div className='gallery'>
-                <p>Gallery</p>
+            <div class="mx-auto video-section">
+                <p>RELIVE <span className='green-color'>2021</span></p>
+                <div className='video'>
+                    <video loop="loop" controls id=''>
+                        <source src={Video2021}></source>
+                    </video>
+                </div>
+                <div className='lineup2021'>
+                    <img src={lineup_2021_1} alt="" />
+                    <img src={lineup_2021_2} alt="" />
+                </div>
             </div>
+
         </div>
     )
 }
